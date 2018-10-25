e5ce27d3f6f9c5c7dbed7a73df1b4afa3663fffd
m.saravia <m.saravia@ISC316.sis.virtual.uniandes.edu.co>
2018-10-24 19:10:33 -0500

    mas cambios

--- src/caso2/Cliente.java
+++ src/caso2/Cliente.java
@@ -63,12 +63,20 @@ public class Cliente {
 	private KeyPair keyPair;
 
 
+	//constructor
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	public Cliente() throws Exception{
 		keyPair = Encriptacion.generacionDeLlaves();
 		certificadoServidor = Encriptacion.crearCertificadoCliente(keyPair);
 	}
 
-
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	private void conectar() throws Exception{
 		socket = new Socket("localhost", 6000);
 		reader = new BufferedReader( new InputStreamReader( socket.getInputStream( ) ) );
@@ -76,15 +84,25 @@ public class Cliente {
 
 	}
 
-	private boolean inicio() throws IOException
-	{
+	/**
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean inicio() throws IOException{
+
 		pw.println("HOLA");
 		String rta = reader.readLine();
 		return rta.equals("OK");
 	}
 
-	private boolean enviarAlgoritmos() throws Exception
-	{
+	/**
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	private boolean enviarAlgoritmos() throws Exception{
+
 		String msj = "ALGORITMOS";
 		for(String s: ALGORITMOS) msj+= ":"+s;
 
@@ -93,26 +111,36 @@ public class Cliente {
 		return rta.equals("ESTADO:OK");
 	}
 
-	private void enviarCertificado() throws Exception
-	{
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	private void enviarCertificado() throws Exception{
+
 		pw.println("CERTCLNT");
 		socket.getOutputStream().write(certificadoCliente.getEncoded());
 		socket.getOutputStream().flush();
 		System.out.println("SRV: "+reader.readLine());
-		System.out.println("SRV: "+reader.readLine());
 		pw.println("ESTADO:OK");
 
-
 	}
 
-	private void recibirCertServer() throws Exception
-	{
-		byte[] bytes2 = new byte[1000];
-		bytes2= Arrays.copyOf(bytes2, socket.getInputStream().read(bytes2));
-		System.out.print(new String(bytes2));
-		certificadoServidor = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(bytes2));
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	private void recibirCertServer() throws Exception{
+
+		byte[] bytes = new byte[1000];
+		bytes= Arrays.copyOf(bytes, socket.getInputStream().read(bytes));
+		System.out.print(new String(bytes));
+		certificadoServidor = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(bytes));
 	}
 
+	/**
+	 * 
+	 * @throws Exception
+	 */
 	private void recibirLlave() throws Exception
 	{
 		String linea = reader.readLine();
@@ -123,6 +151,11 @@ public class Cliente {
 		secretKey =  new SecretKeySpec(llave, 0, llave.length, ALGORITMOS[1]);
 	}
 
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
 	private String pasarAString(byte[] data)
 	{
 		String rta = "";
@@ -131,6 +164,11 @@ public class Cliente {
 		return rta;
 	}
 
+	/**
+	 * 
+	 * @param data
+	 * @return
+	 */
 	private byte[] extraer(String data)
 	{
 		byte[] rta = new byte[data.length() / 2];
@@ -138,7 +176,13 @@ public class Cliente {
 			rta[i] = Integer.decode("#"+data.substring(i * 2, (i + 1) * 2)).byteValue();
 		return rta;
 	}
-	
+
+	/**
+	 * 
+	 * @param posicion
+	 * @return
+	 * @throws Exception
+	 */
 	public boolean enviarPosicion(String posicion) throws Exception
 	{	
 		conectar();
@@ -147,21 +191,24 @@ public class Cliente {
 		enviarCertificado();
 		recibirCertServer();
 		recibirLlave();
-		
-		
-		byte[] cipher = Encriptacion.encriptar(posicion.getBytes(),secretKey, ALGORITMOS[0]+PADDING);
-		pw.println("ACT1:"+pasarAString(cipher));
-		
-		
+
+		byte[] cifrado = Encriptacion.encriptar(posicion.getBytes(),secretKey, ALGORITMOS[0]+PADDING);
+		pw.println("ACT1:"+pasarAString(cifrado));
+
 		byte[] mac = Encriptacion.calculoDelMac(posicion.getBytes(), secretKey, ALGORITMOS[2]);
 		pw.println("ACT2:"+pasarAString(Encriptacion.encriptar(mac, certificadoServidor.getPublicKey(),ALGORITMOS[1])));
-		
+
 		String linea = reader.readLine();
-		
-		
+
+
 		return linea.equals("ESTADO:OK");
 	}
 
+	//main
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		try{
 			Cliente c = new Cliente();
