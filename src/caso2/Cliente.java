07d2d1d7ee1093fa08b0eacc3889bece3b300d92
MATEO SARAVIA SALAMANCA <m.saravia@sis.virtual.uniandes.edu.co>
2018-10-24 19:00:38 -0500

    cambios idiotas

--- src/caso2/Cliente.java
+++ src/caso2/Cliente.java
@@ -15,117 +15,153 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Cliente {
-	private final static String[] ALGORITMOS = {"AES","RSA","HMACSHA1"};
+
+	//atributos
+	/**
+	 * 
+	 */
 	private final static String PADDING="/ECB/PKCS5Padding";
+
+	/**
+	 * 
+	 */
+	private final static String[] ALGORITMOS = {"AES", "RSA", "HMACMD5"};
+
+	/**
+	 * 
+	 */
+	private static PrintWriter pw;
+
+	/**
+	 * 
+	 */
+	private SecretKey secretKey;
+
+	/**
+	 * 
+	 */
+	private X509Certificate certificadoServidor;
+
+	/**
+	 * 
+	 */
+	private X509Certificate certificadoCliente;
+
+	/**
+	 * 
+	 */
+	private BufferedReader reader;
+
+	/**
+	 * 
+	 */
 	private Socket socket;
-	private BufferedReader reader; 
-	private static PrintWriter writer;
-	private SecretKey llaveSesion;
-	private X509Certificate serverCert;
-	private X509Certificate clientCert;
-	private KeyPair kp;
-	
-	
+
+	/**
+	 * 
+	 */
+	private KeyPair keyPair;
+
+
 	public Cliente() throws Exception{
-		kp = Encriptacion.generacionDeLlaves();
-		clientCert = Encriptacion.crearCertificadoCliente(kp);
+		keyPair = Encriptacion.generacionDeLlaves();
+		certificadoServidor = Encriptacion.crearCertificadoCliente(keyPair);
 	}
-	
-	public boolean enviarPosicion(String posicion) throws Exception
-	{	
-		conectar();
-		inicar();
-		enviarAlgoritmos();
-		enviarCertificado();
-		recibirCertServer();
-		recibirLlave();
-		
-		
-		byte[] cipher = Encriptacion.encriptar(posicion.getBytes(),llaveSesion, ALGORITMOS[0]+PADDING);
-		writer.println("ACT1:"+pasarAString(cipher));
-		
-		
-		byte[] mac = Encriptacion.calculoDelMac(posicion.getBytes(), llaveSesion, ALGORITMOS[2]);
-	    writer.println("ACT2:"+pasarAString(Encriptacion.encriptar(mac, serverCert.getPublicKey(),ALGORITMOS[1])));
-	    
-	    String linea = reader.readLine();
-	   
-	    
-	    return linea.equals("ESTADO:OK");
-	}
-	
+
+
 	private void conectar() throws Exception{
 		socket = new Socket("localhost", 6000);
 		reader = new BufferedReader( new InputStreamReader( socket.getInputStream( ) ) );
-		writer = new PrintWriter( socket.getOutputStream( ), true );
-		
+		pw = new PrintWriter( socket.getOutputStream( ), true );
+
 	}
-	
-	private boolean inicar() throws IOException
+
+	private boolean inicio() throws IOException
 	{
-		writer.println("HOLA");
+		pw.println("HOLA");
 		String rta = reader.readLine();
-		return rta.equals("INICIO");
+		return rta.equals("OK");
 	}
-	
+
 	private boolean enviarAlgoritmos() throws Exception
 	{
-		String msj = null;
-		for(String s: ALGORITMOS)
-			if(msj==null)
-				msj="ALGORITMOS:"+s;
-			else msj+= ":"+s;
-		writer.println(msj);
+		String msj = "ALGORITMOS";
+		for(String s: ALGORITMOS) msj+= ":"+s;
+
+		pw.println(msj);
 		String rta=  reader.readLine();
 		return rta.equals("ESTADO:OK");
 	}
-	
+
 	private void enviarCertificado() throws Exception
 	{
-		writer.println("CERTCLNT");
-		socket.getOutputStream().write(clientCert.getEncoded());
+		pw.println("CERTCLNT");
+		socket.getOutputStream().write(certificadoCliente.getEncoded());
 		socket.getOutputStream().flush();
 		System.out.println("SRV: "+reader.readLine());
 		System.out.println("SRV: "+reader.readLine());
-		writer.println("ESTADO:OK");
-		
-		
+		pw.println("ESTADO:OK");
+
+
 	}
-	
+
 	private void recibirCertServer() throws Exception
 	{
 		byte[] bytes2 = new byte[1000];
 		bytes2= Arrays.copyOf(bytes2, socket.getInputStream().read(bytes2));
 		System.out.print(new String(bytes2));
-		serverCert = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(bytes2));
+		certificadoServidor = (X509Certificate)CertificateFactory.getInstance("X.509").generateCertificate(new ByteArrayInputStream(bytes2));
 	}
-	
+
 	private void recibirLlave() throws Exception
 	{
 		String linea = reader.readLine();
-        byte[] data = extraer(linea.split(":")[1]);
-        
-	    byte[] llave = Encriptacion.decriptar(data , kp.getPrivate(),ALGORITMOS[1]);
-	    
-		llaveSesion =  new SecretKeySpec(llave, 0, llave.length, ALGORITMOS[1]);
+		byte[] data = extraer(linea.split(":")[1]);
+
+		byte[] llave = Encriptacion.decriptar(data , keyPair.getPrivate(),ALGORITMOS[1]);
+
+		secretKey =  new SecretKeySpec(llave, 0, llave.length, ALGORITMOS[1]);
 	}
 
 	private String pasarAString(byte[] data)
 	{
 		String rta = "";
 		for (byte b: data)
-			 rta+= String.format("%2s",Integer.toHexString((char)b & 0xFF)).replace(' ', '0');
+			rta+= String.format("%2s",Integer.toHexString((char)b & 0xFF)).replace(' ', '0');
 		return rta;
 	}
 
 	private byte[] extraer(String data)
 	{
 		byte[] rta = new byte[data.length() / 2];
-	    for (int i = 0; i < rta.length; i++) 
-	    	rta[i] = Integer.decode("#"+data.substring(i * 2, (i + 1) * 2)).byteValue();
-	    return rta;
+		for (int i = 0; i < rta.length; i++) 
+			rta[i] = Integer.decode("#"+data.substring(i * 2, (i + 1) * 2)).byteValue();
+		return rta;
 	}
 	
+	public boolean enviarPosicion(String posicion) throws Exception
+	{	
+		conectar();
+		inicio();
+		enviarAlgoritmos();
+		enviarCertificado();
+		recibirCertServer();
+		recibirLlave();
+		
+		
+		byte[] cipher = Encriptacion.encriptar(posicion.getBytes(),secretKey, ALGORITMOS[0]+PADDING);
+		pw.println("ACT1:"+pasarAString(cipher));
+		
+		
+		byte[] mac = Encriptacion.calculoDelMac(posicion.getBytes(), secretKey, ALGORITMOS[2]);
+		pw.println("ACT2:"+pasarAString(Encriptacion.encriptar(mac, certificadoServidor.getPublicKey(),ALGORITMOS[1])));
+		
+		String linea = reader.readLine();
+		
+		
+		return linea.equals("ESTADO:OK");
+	}
+
 	public static void main(String[] args) {
 		try{
 			Cliente c = new Cliente();
