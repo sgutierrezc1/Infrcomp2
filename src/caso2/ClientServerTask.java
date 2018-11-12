0deca5dd229ae8a2e32419cf3f9cb8167c7d37a1
Mateo Saravia <m.saravia@uniandes.edu.co>
2018-11-11 22:27:56 -0500

    gload

--- src/caso2/ClientServerTask.java
+++ src/caso2/ClientServerTask.java
@@ -20,9 +20,13 @@ public class ClientServerTask extends Task{
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		Client client= new Client();
-		client.sendMessageToServer("Hi! I'm a client");
-		client.waitForMessageFromServer();
+		try {
+			Cliente cliente= new Cliente();
+			cliente.enviarPosicion("41 24.2028, 2 10.4418");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			fail();
+		}
 	}
 
 }
